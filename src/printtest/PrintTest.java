86c542c1a135236af481efc7b3232eca5e1b36af
daehyeonkwon <hyun97130@naver.com>
2022-03-29 12:20:16 +0900

    PrintTest 타입별 오버로딩

--- src/printtest/PrintTest.java
+++ src/printtest/PrintTest.java
@@ -5,5 +5,13 @@ public class PrintTest {
 		System.out.println(str);
 	}
 	// 타입별 메소드 오버로딩
-	
+	public static void printTest(int i) {
+		System.out.println(i);
+	}
+	public static void printTest(char c) {
+		System.out.println(c);
+	}
+	public static void printTest(double d) {
+		System.out.println(d);
+	}
 }
