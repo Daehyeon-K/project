63cd112425b198b41294f856b3d0b13a84854c5b
youngjaejung <jyj314@gmail.com>
2022-03-29 12:14:35 +0900

    연습용

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -1,8 +1,15 @@
 package basic;
 
+import java.util.Scanner;
+
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("어려워요");
+		System.out.println("난 뭘하고있는걸까...");
+		System.out.println("나는 타자기다...");
+		int haha;
+		Scanner sc = new Scanner(System.in);
+		
 
 	}
 }
